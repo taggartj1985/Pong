22bff816187ceebd09e963b596610c0ee5478530
taggartj1985 <taggartj1985@gmail.com>
2021-02-02 12:53:54 +0000

    added title screen

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import Phaser from 'phaser'
 
+import TitleScreen from './scenes/TitleScreen'
+
 const config = {
     width: 800,
     height: 500,
@@ -7,3 +9,7 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
+
+game.scene.add('titlescreen', TitleScreen)
+
+game.scene.start('titlescreen')
